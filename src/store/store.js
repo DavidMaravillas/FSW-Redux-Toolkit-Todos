0465b94e63224565020ea8865a3d7f5c20b70638
david maravillas <davidmaravillas@pursuit.org>
2020-04-19 10:54:01 -0400

    add new store

---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import { configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+// import fruits from "../features/fruitStand/fruitStandSlice";
+// import farmers from "../features/farmers/farmersSlice";
+
+const reducer = {
+  
+};
+
+const middleware = [...getDefaultMiddleware(), logger];
+
+export default configureStore({
+    reducer, 
+    middleware
+});
